d6a9f3fb16b9a4c9ccfc372643a37507d7cc0972
aleenavarghese29 <aleenavar29@gmail.com>
2025-05-28 18:47:44 +0530

    Fix NLTK data path issue and finalize Streamlit app

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 # test change for git
 import nltk
+import os
 
-def download_nltk_resources():
-    resources = ['punkt', 'stopwords', 'wordnet']
-    for resource in resources:
-        try:
-            nltk.data.find(f'tokenizers/{resource}' if resource == 'punkt' else f'corpora/{resource}')
-        except LookupError:
-            nltk.download(resource)
+# Set up a persistent NLTK data directory
+nltk_data_dir = os.path.join(os.path.expanduser("~"), "nltk_data")
+os.makedirs(nltk_data_dir, exist_ok=True)
+nltk.download('punkt', download_dir=nltk_data_dir)
+nltk.download('stopwords', download_dir=nltk_data_dir)
+nltk.download('wordnet', download_dir=nltk_data_dir)
+nltk.data.path.append(nltk_data_dir)  # Make sure NLTK uses this path
 
-download_nltk_resources()
 
 import os
 import re
